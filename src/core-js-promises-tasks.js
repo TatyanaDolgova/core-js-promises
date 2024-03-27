34f56e3145275fbeaa11a6f9b1d12c6f477317d7
Tatyana Dolgova <kylikovatatyana1993@mail.ru>
2024-03-27 09:47:53 +0500

    feat: complete some tasks

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -102,8 +102,8 @@ function getFirstPromiseResult(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return Promise.all(promises);
 }
 
 /**
@@ -118,8 +118,17 @@ function getAllOrNothing(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises)
+    .then(results => {
+      return results.map(result => {
+        if (result.status === 'fulfilled') {
+          return result.value;
+        } else {
+          return null;
+        }
+      })
+    })
 }
 
 /**
